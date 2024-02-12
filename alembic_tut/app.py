492c2c536af85254356aad95d4712fa6838989a5
dinoDayo <dayo1523@gmail.com>
2024-02-12 11:12:45 -0500

    Alembic tutorial completed. I understand now

---
+++ alembic_tut/app.py
@@ -0,0 +1,52 @@
+import uvicorn
+from starlette.applications import Starlette
+from starlette.endpoints import HTTPEndpoint
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
+from tutorial_dto import Tutorial
+from services import (
+    list_tutorials,
+    save_tutorial,
+)
+
+
+class TutorialEndpoints(HTTPEndpoint):
+    async def get(self, request):
+        tutorials = list_tutorials()
+        return JSONResponse([
+            {
+                "tutorial_id": tutorial.tutorial_id,
+                "name": tutorial.name,
+            }
+            async for tutorial in tutorials
+        ])
+
+
+    async def post(self, request):
+        data = await request.json()
+        name = data["name"]
+        tutorial = await save_tutorial(Tutorial(name=name))
+
+        return JSONResponse({
+            "tutorial_id": tutorial.tutorial_id,
+            "name": tutorial.name,
+        })
+
+
+def create_app():
+    app = Starlette(
+        debug=True,
+        routes=[
+            Route("/", TutorialEndpoints),
+        ],
+    )
+
+    return app
+
+
+app = create_app()
+
+
+# if __name__ == "__main__":
+#     uvicorn.run("app.main:app", host="0.0.0.0", port=9000, reload=True)
---
+++ alembic_tut/database.py
@@ -0,0 +1,48 @@
+"""
+This file provides a schema for alembic to apply to the postgres database
+"""
+import os
+from contextlib import asynccontextmanager
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+)
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = create_async_engine(
+    os.environ["postgres_url"],
+    echo=True,
+    future=True,
+)
+
+Base = declarative_base()
+
+
+class Tutorial(Base):
+    __tablename__ = "tutorials"
+
+    tutorial_id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+
+
+def async_session_generator():
+    return sessionmaker(
+        engine, class_=AsyncSession
+    )
+
+
+@asynccontextmanager
+async def get_session():
+    try:
+        async_session = async_session_generator()
+
+        async with async_session() as session:
+            yield session
+    except:
+        await session.rollback()
+        raise
+    finally:
+        await session.close()
